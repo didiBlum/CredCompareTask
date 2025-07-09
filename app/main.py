8a5035abd0b0ae759759ee7a066a0f391ac01972
Didi Blum <didiblum@Didis-MacBook-Pro.local>
2025-07-09 16:50:32 +0700

    Fix duplicate main files.

--- app/main.py
+++ app/main.py
@@ -4,9 +4,4 @@ from app.api.routes import router
 
 app = FastAPI()
 
-@app.get("/")
-async def hello_world():
-    await asyncio.sleep(0)
-    return {"message": "hello world"}
-
 app.include_router(router) 
